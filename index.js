052dfcad1cd449128e5f4e3a973ba1e8ad771ff1
thistonyb <this.tonybaker@gmail.com>
2020-01-10 17:05:41 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-'\s]+/g, "");
+  }
+  static titleize(string) {
+    const lowerCase = [
+      "the",
+      "a",
+      "an",
+      "but",
+      "of",
+      "and",
+      "for",
+      "at",
+      "by",
+      "from"
+    ];
+    const splitArray = string.split(" ");
+    const comparisonArray = [
+      this.capitalize(splitArray[0]),
+      ...splitArray.slice(1)
+    ];
+    return comparisonArray
+      .map(word => {
+        return lowerCase.includes(word) ? word : this.capitalize(word);
+      })
+      .join(" ");
+  }
 }
