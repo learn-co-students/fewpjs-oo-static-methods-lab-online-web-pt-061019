2ed82adb71f500dcb2b3d86cc14381092acb14e8
Sam Markham <sam.e.markham@gmail.com>
2020-02-06 11:43:20 -0800

    Done.

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ class Formatter {
   static titleize(string) {
     const arr = string.split(" ");
     const fixedArr = arr.map(function(el) {
-      console.log(el);
+      // console.log(el);
       if (el === arr[0]) {
         // console.log(el);
         return this.capitalize(el);
       }
       else if (el !== 'the' && el !== 'a' && el !== 'an' && el !== 'but' && el !== 'of' && el !== 'and' && el !== 'for' && el !== 'at' && el !== 'by' && el !== 'from') {
+        //or: !["a", "the"].includes(el)
         return this.capitalize(el);
       } else {
         return el;
