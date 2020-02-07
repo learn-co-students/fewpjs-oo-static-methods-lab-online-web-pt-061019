cbcc61317667ce384a372f5ebb6099a64d3f5367
scrabill <shannon.crabill@gmail.com>
2020-02-07 17:20:30 -0500

    Done.

--- index.js
+++ index.js
@@ -5,18 +5,51 @@ class Formatter {
   }
 
   static sanitize(string) {
-    return string.replace(/[^- ''']+/g, '');
+    return string.replace(/[^A-Za-z\s-']+/g, '');
   }
 
   static titleize(string) {
+    // Write a method static titleize that takes in a string and capitalizes all words
+    // in a sentence except the, a, an, but, of, and, for, at, by, and from;
+    // and always capitalizes the first word.
+
+    let exceptions = ["the","a", "an", "but", "of", "and", "for", "at", "by", "from"]
     let words = string.split(' ')
-    let firstWord = words[0]
-    let shout = firstWord.toUpperCase()
-    let newWords = words.unshift(shout)
+    let titlizedArray = []
 
-    let newString = newWords.join(' ')
+    words.forEach((word, i) => {
 
-    return newString
+      // For each word that is pass into the loop, see if it is one of the exception words
+      let result = exceptions.find( function(x) { return word === x })
+
+      // If it's the first word, capitalize the first letter and move on
+      // If it's not the first word, but is a reserved word, add it to the array without changing it
+      // Else, capitalize the first character only
+
+      if (i == 0) {
+
+        let newWord = word.charAt(0).toUpperCase() + word.slice(1)
+        titlizedArray.push(newWord)
+        return titlizedArray
+
+      } else if (result) {
+
+        titlizedArray.push(word)
+        return titlizedArray
+
+      } else {
+
+        let newWord = word.charAt(0).toUpperCase() + word.slice(1)
+        titlizedArray.push(newWord)
+        return titlizedArray
+
+      }
+
+    });
+
+    let titlizedString = titlizedArray.join(' ')
+
+    return titlizedString
 
   }
 }
