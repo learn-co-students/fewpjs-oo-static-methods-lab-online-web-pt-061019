b1355e47df1b1bc1f7f0419ef423df2bffe285e4
github username <github email address>
2020-02-10 10:50:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9- ']+/g, '');
+  }
+
+  static titleize(string) {
+    let nonCapitalize = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let splitString = string.split(" ")
+    let stringResult = [this.capitalize(splitString[0])]
+    splitString = splitString.slice(1)
+    for(const word of splitString) {
+      if (!nonCapitalize.find(el => el === word)){
+        stringResult.push(this.capitalize(word))
+      } else {
+        stringResult.push(word)
+      }
+    }
+    return stringResult.join(" ");
+  }
 }
