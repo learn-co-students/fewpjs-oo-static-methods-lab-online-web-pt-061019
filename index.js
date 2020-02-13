853cee13e1317dacb63fab13ce53f0464712725d
maasa333 <konomaasa@gmail.com>
2020-02-12 19:50:04 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string) {
+    const firstWord = this.capitalize(string.split(' ')[0]);
+
+    const downCaseWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = string.split(' ').slice(1);
+    const upCaseWords = words.map(word => {
+      if (!downCaseWords.includes(word)) {
+        return this.capitalize(word);
+      } else {
+        return word;
+      }
+    }).join(" ")
+    return firstWord + " " + upCaseWords;
+  }
 }
+
+Formatter.titleize( "where the wild things are" )
+Formatter.titleize( "chicken soup with rice and a few other songs" )
+Formatter.titleize( "a tale of two cities" )
