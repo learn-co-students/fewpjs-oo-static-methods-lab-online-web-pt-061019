5d0dbc6651803dd7fa234a4cc35cd2cc1d3dd559
tshuttles <tshuttlesworth@gmail.com>
2020-02-19 19:24:24 -0600

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+  static titleize(string){
+    let splitString = string.split(' ')
+    let titleString = []
+    let exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+    for(let i = 0; i < splitString.length; i++){
+      if (i == 0) {
+        titleString.push(this.capitalize(splitString[i]))
+      } else {
+        if (exceptions.includes(splitString[i])){
+          titleString.push(splitString[i])
+        } else {
+          titleString.push(this.capitalize(splitString[i]))
+        }
+      }
+    }
+  return titleString.join(' ')
+  }
 }
